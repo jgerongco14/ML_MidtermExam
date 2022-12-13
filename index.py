da03ae23ea133876352596b7ff811fc4086f8ca9
Rendel Jeff A. Gerongco <jgerongco14@gmail.com>
2022-12-14 00:11:39 +0800

    update
    mugana na

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask,render_template,request
-# ERROR IMPORTS
 import joblib
 import numpy as np
 
@@ -9,8 +8,8 @@ app = Flask(__name__)
 def myapp():
     return render_template('index.html')
 
-naive_model = joblib.load('naivebayes_model.joblib')
-dectree_model = joblib.load('decision_tree_model.joblib')
+naive_model = joblib.load('assets\\naivebayes_model.joblib')
+dectree_model = joblib.load('assets\\decision_tree_model.joblib')
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -30,15 +29,25 @@ def test():
 
     model_class = request.form['model']
 
-    # DILI PA SURE
+    # prediction
     if model_class == 'naive_bayes' :
          pred = naive_model.predict(ans)
+    elif model_class == 'decision_tree' :
+         pred = dectree_model.predict(ans)
+
+
+    # convert numeric prediction to word
+    # 0 = no diabetes 1 = prediabetes 2 = Has Diabetes
+    if pred[0] == 0:
+        output = 'No Diabetes'
+    elif pred[0] == 1:
+        output = 'Prediabetes'
+    elif pred[0] == 2:
+        output = 'Has Diabetes'
     
-    return render_template('result.html',answer= ans, class_model = model_class, predict=pred)
+    
+    return render_template('result.html',answer= ans, class_model = model_class, predict=output)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 
